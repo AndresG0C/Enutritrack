975d076cc83a67a0c1a84e8d8cad1b53ace42ee2
AndresG0C <andresgonzalez22004@gmail.com>
2026-06-01 15:58:14 -0500

    reestructuración de terraform

--- Terraform/modules/security/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  type        = string
-  description = "Nombre del proyecto enviado desde la raiz"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "ID de la VPC enviado desde el modulo vpc"
-}
